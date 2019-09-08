8ad4c84aa941dfc7b7bec8bd0166ff1b7c17cd2f
Edelcio Molina <edelciomolina@gmail.com>
2019-09-08 17:49:53 -0300

    Module Export and Test

--- index.js
+++ index.js
@@ -1,122 +1,145 @@
-void (function(root) {
-  const fs = require("fs");
-  const util = require("util");
-  const nrc = require("node-run-cmd");
-  const express = require("express");
-  const services = {};
+const fs = require("fs");
+const util = require("util");
+const nrc = require("node-run-cmd");
+const express = require("express");
 
-  let serverPort = 3000;
-  let serverDone = false;
+let services = {};
+let serverPort = 3000;
+let serverDone = false;
+let httpServer = null;
 
-  const serveFirebase = async () => {
-    let timer = setTimeout(() => {});
-    nrc.run("cmd.exe /c firebase serve", {
-      onData: data => {
-        console.log(data);
-        if (!serverDone) {
-          clearTimeout(timer);
-          timer = setTimeout(() => {
-            serverDone = true;
-            return;
-          }, 2000);
-        }
+const serveFirebase = async () => {
+  let timer = setTimeout(() => {});
+  nrc.run("cmd.exe /c firebase serve", {
+    onData: data => {
+      console.log(data);
+      if (!serverDone) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          serverDone = true;
+          return;
+        }, 2000);
       }
+    },
+    onError: data => {
+      if (!!data.trim()) {
+        console.log(data);
+        serverDone = true;
+        return;
+      }
+    }
+  });
+
+  const resolveAfterDone = () => {
+    return new Promise(resolve => {
+      const interval = setInterval(() => {
+        if (serverDone) {
+          clearInterval(interval);
+          resolve();
+        }
+      }, 100);
     });
-
-    const resolveAfterDone = () => {
-      return new Promise(resolve => {
-        const interval = setInterval(() => {
-          if (serverDone) {
-            clearInterval(interval);
-            resolve();
-          }
-        }, 100);
-      });
-    };
-    await resolveAfterDone();
   };
+  await resolveAfterDone();
+};
 
-  const runExpress = async () => {
-    const app = express();
-    const config = await getFirebaseJson();
-    const message = `Functions Running at ${serverPort}`;
-    try {
-      await config.hosting.rewrites.forEach(item => {
-        item.function && app.get(item.source, services[item.function]);
-      });
-      app.get("/", (req, res) => res.send(message));
-      await app.listen(serverPort);
-      console.info(message);
-    } catch (error) {}
-  };
+const runExpress = async () => {
+  const app = express();
+  const config = await getFirebaseJson();
+  const message = `>>> Functions Running at ${serverPort} <<<`;
+  try {
+    await config.hosting.rewrites.forEach(item => {
+      item.function && app.get(item.source, services[item.function]);
+    });
+    app.get("/", (req, res) => res.send(message));
+    httpServer = await app.listen(serverPort);
+    console.info(message);
+  } catch (error) {}
+};
 
-  const getFirebaseJson = async suffix => {
+const getFirebaseJson = async suffix => {
+  try {
     const readFile = util.promisify(fs.readFile);
     const contents = await readFile(`firebase.json${suffix || ""}`, "utf8");
     return !!contents ? JSON.parse(contents) : {};
-  };
+  } catch (error) {
+    return {};
+  }
+};
 
-  const saveFirebaseJson = async (config, suffix) => {
-    const writeFile = util.promisify(fs.writeFile);
-    return await writeFile(
-      `firebase.json${suffix || ""}`,
-      JSON.stringify(config, null, 2)
-    );
-  };
+const saveFirebaseJson = async (config, suffix) => {
+  const writeFile = util.promisify(fs.writeFile);
+  return await writeFile(
+    `firebase.json${suffix || ""}`,
+    JSON.stringify(config, null, 2)
+  );
+};
 
-  const backupFirebaseJson = async () => {
-    let config = await getFirebaseJson();
-    if (!config.backup) {
-      await recoverFirebaseJson();
-      config = await getFirebaseJson();
-    }
-    config.backup = new Date();
-    await saveFirebaseJson(config, ".bkp");
-  };
+const deleteFirebaseJsonBackup = async () => {
+  const unlink = util.promisify(fs.unlink);
+  return await unlink("firebase.json.bkp");
+};
 
-  const replaceFirebaseJson = async () => {
-    let config = await getFirebaseJson();
-    await config.hosting.rewrites.forEach(item => {
-      if (item.function) {
-        config.hosting.redirects = config.hosting.redirects || [];
-        config.hosting.redirects.push({
-          source: item.source,
-          destination: `http://localhost:${serverPort}${item.source}`
-        });
-
-        const deleteByValue = (obj, val) => {
-          for (let index = 0; index < obj.length; index++) {
-            const element = obj[index];
-            if (element.source == val) {
-              delete obj[index];
-            }
-          }
-        };
-
-        deleteByValue(config.hosting.rewrites, item.source);
-      }
-    });
-    await saveFirebaseJson(config);
-  };
-
-  const recoverFirebaseJson = async () => {
-    let config = await getFirebaseJson(".bkp");
-    delete config.backup;
-    await saveFirebaseJson(config);
-  };
-
-  const run = async config => {
-    services = config.services || {};
-    serverPort = config.port || serverPort;
-
-    await runExpress();
-    await backupFirebaseJson();
-    await replaceFirebaseJson();
-    await serveFirebase();
+const backupFirebaseJson = async () => {
+  let config = await getFirebaseJson();
+  if (config.backup) {
     await recoverFirebaseJson();
-  };
+    config = await getFirebaseJson();
+  }
+  config.backup = new Date();
+  await saveFirebaseJson(config, ".bkp");
+};
 
-  module.exports = {
-    run
-  };
-})(this);
+const replaceFirebaseJson = async () => {
+  let config = await getFirebaseJson();
+  await config.hosting.rewrites.forEach(item => {
+    if (item && item.function) {
+      config.hosting.redirects = config.hosting.redirects || [];
+      config.hosting.redirects.push({
+        source: item.source,
+        destination: `http://localhost:${serverPort}${item.source}`
+      });
+
+      const deleteByValue = (obj, val) => {
+        for (let index = 0; index < obj.length; index++) {
+          const element = obj[index];
+          if (element.source == val) {
+            delete obj[index];
+          }
+        }
+      };
+
+      deleteByValue(config.hosting.rewrites, item.source);
+    }
+  });
+  await saveFirebaseJson(config);
+};
+
+const recoverFirebaseJson = async () => {
+  let config = await getFirebaseJson(".bkp");
+  delete config.backup;
+  await saveFirebaseJson(config);
+};
+
+const stop = async () => {
+  httpServer.getConnections(() => {});
+  httpServer.close();
+};
+
+const run = async config => {
+  services = config.services || {};
+  serverPort = config.port || serverPort;
+
+  await runExpress();
+  await backupFirebaseJson();
+  await replaceFirebaseJson();
+  await serveFirebase();
+  await recoverFirebaseJson();
+  await deleteFirebaseJsonBackup();
+  return { running: true };
+};
+
+module.exports = {
+  run,
+  stop
+};
--- test/test.js
+++ test/test.js
@@ -1,1 +1,8 @@
-void (function() {})();
+void (function() {
+  const ffr = require("../index");
+  const services = require("../functions/services");
+  const port = 3000;
+  ffr.run({ services, port }).then(state => {
+    ffr.stop();
+  });
+})();
